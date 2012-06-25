0418e46dbea60f309824b48fdc950960e8bade02
Dexter Taylor <dtaylor@iron.local>
2012-06-25 17:55:31 -0400

    ...

--- bootstrap/scripts/serpentine.js
+++ bootstrap/scripts/serpentine.js
@@ -139,25 +139,49 @@ function registerAction(actionMap, actionName, actionURL, dataValues, selectionM
     actionMap[actionName] = { 'url': actionURL, 'data': valuesDict, 'must_select': selectionMandatory };    
 }
 
+
+function setFormVariableOnAction(formVarMap, actionName, formVarID, value){
+
+    formVarMap[actionName] = {'id': formVarID, 'value': value};
+}
+
     
 // Universal logic for managing the dropdown menu/data table UI pattern.
 // The actionMap parameter is a dictionary (indexed by action name) that holds 
 // the data parameters for table selection logic:
-// 'url': target URL (w/optional embedded variables), 
+// 'target': function reference or target URL (w/optional embedded variables), 
 // 'data': the variables themselves, and 
 // 'must_select': a boolean
 // signifying whether the selected action requires a row from the table to be selected as well.
 // 
-function initTableSelectLogic(actionMap){
+function initTableSelectLogic(actionMap, formVarMap){
 
         /* 
         In the click handler for the go button, we index into the actionMap hashtable
         using the value of the action_selector dropdown.
         */
         $("#go_button").click(function(){
-
+        
             action = $("#action_selector option:selected").attr("id");
             selectedObject = $("input.object_id_button:checked").attr("value"); 
+        
+            if(formVarMap != null){
+            
+                sLog("selected action is: " + action);
+                
+                // set form variables according to the selected user action                
+                formVarData = formVarMap[action];
+                if(formVarData != null){
+                    targetValue = formVarData['value'];
+                    if(typeof targetValue == "function"){
+                        $("#" + formVarData["id"]).attr("value", targetValue());
+                    }  
+                    else{
+                        $("#" + formVarData["id"]).attr("value", targetValue);
+                    }
+                }                
+            }
+
             
             // does this action require a selection?
             if(actionMap[action]['must_select'] && selectedObject == null){
@@ -165,47 +189,104 @@ function initTableSelectLogic(actionMap){
                 return false;           
             }
             else{
-                url = actionMap[action]['url'];
-                // find and resolve embedded variables in the URL,
-                // which are specified as an alphanumeric string between
-                // curly brackets 
-                //
-                var re = new RegExp("{[a-zA-Z]*}");
-                matches = re.exec(url);
+            
+                if(typeof actionMap[action]['target'] === "function"){
+                    // clients may pass a raw function reference
+                    actionMap[action]['target']();
+                    return false;
+                }
+                else if(new RegExp("([a-zA-Z\\_\\-]+\\((\\{[a-zA-Z\\_\\-]+\\}){1}\\))").test(actionMap[action]['target'])){ 
+                    // We also autodetect functions;
+                    // if the target is a string containing a name followed by parentheses 
+                    // enclosing a set of matched curly brackets, like this:
+                    //
+                    // foo({var})
+                    //
+                    // we interpret it as a function containing a variable argument.
                 
-                if(matches != null){
+                    sLog("Registered a function with one or more embedded variables.");
                     
-                    for (index = 0; index < matches.length; index++) {
-                        newVar = matches[index];
-                        key = newVar.slice(1, newVar.length-1) // gives us the string btwn the curly brackets
+                    targetString = actionMap[action]['target'];
+                    funcName = targetString.slice(0, targetString.indexOf("("));
+                    
+                    re = new RegExp("{[a-zA-Z\\_\\-]+}");
+                    matches = re.exec(targetString);
+                    newVar = matches[0];
+                    key = newVar.slice(1, newVar.length-1).killWhitespace(); // gives us the string btwn the curly brackets
                         
-                        sLog("retrieved embedded variable " + key);
-                        // the value of the var is supplied by whatever data we passed
-                        // to registerAction in a dictionary, with named variables as keys
-                        value = null;
-                        
-                        obj = actionMap[action]['data'][key]; 
-                        if(obj === null){
-                            continue;
-                        }
-                        else if(typeof obj === "function"){                            
+                    sLog("Retrieved embedded variable: " + key);
+                    
+                    // the value of the var is supplied by whatever data we passed
+                    // to registerAction() in the dataValues parameter, with named variables as keys
+                    value = null;
+                    
+                    obj = actionMap[action]['data'][key]; 
+                    if(obj != null){
+                        if(typeof obj === "function"){                            
                             value = obj();  // if it's a function, call it                            
                         }
                         else{
                             value = obj;    // otherwise just assign
                         }
-                        
-                        if(value != null){
-                            newURL = actionMap[action]['url'].replace(newVar, value);
-                            actionMap[action]['url'] = newURL;
-                        }
                     }
+                    
+                    sLog("Calling dynamic function " + funcName);                    
+                    window[funcName](value);                    
+                    
+                    return false;
                 }
-                
+                else{
+                    // if the target is not a function reference, we assume it's a URL
+                    
+                    url = actionMap[action]['target'];
+                    // find and resolve embedded variables in the URL,
+                    // which are specified as an alphanumeric string between
+                    // curly brackets 
+                    //
+                    var re = new RegExp("{[a-zA-Z]+}");
+                    matches = re.exec(url);
+                    
+                    if(matches != null){
+                    
+                        sLog("Registered a URL with one or more embedded variables.");
+                        
+                        for (index = 0; index < matches.length; index++) {
+                            newVar = matches[index];
+                            key = newVar.slice(1, newVar.length-1).killWhitespace(); // gives us the string btwn the curly brackets
+                            
+                            sLog("Retrieved embedded variable: " + key);
+                            
+                            // the value of the var is supplied by whatever data we passed
+                            // to registerAction() in the dataValues parameter, with named variables as keys
+                            value = null;
+                            
+                            obj = actionMap[action]['data'][key]; 
+                            if(obj === null){
+                                continue;
+                            }
+                            else if(typeof obj === "function"){                            
+                                value = obj();  // if it's a function, call it                            
+                            }
+                            else{
+                                value = obj;    // otherwise just assign
+                            }
+                            
+                            sLog("Embedded URL variable " + key + " has value: " + value);
+                            
+                            if(value != null){
+                                newURL = url.replace(newVar, value);
+                                actionMap[action]['target'] = newURL;
+                            }
+                            
+                            sLog("updated target URL is " + actionMap[action]['target']);
+                            sLog("asset ID in form is " + $("#asset_id").attr("value"));
+                        }                    
+                    }  
+                    
+                    $("#action_form").attr("action", actionMap[action]['target']);  
+                    return true;
+                }           
             }
-            //alert(actionMap[action]['url']);
-            $("#action_form").attr("action", actionMap[action]['url']);
-            
-            return true;
+
         });
 }
